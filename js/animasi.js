32923fd0027e881b7ffb70caaa792a1984d68643
risyanz <repirisyan@gmail.com>
2019-03-17 15:19:28 +0700

    commit3

--- js/animasi.js
+++ js/animasi.js
@@ -1,20 +1,30 @@
-$(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 150) { // check if user scrolled more than 50 from top of the browser window
-        $(".navbar-fixed-top").css("background-color", "#0d47a1"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-      } else {
-        $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+
+  var stop=true;
+  $(document).ready(function(){
+    $(window).scroll(function() { 
+      if ($(this.document).scrollTop() > 150) { 
+        $(".carousel-caption").css("opacity", "0.5"); 
+      } 
+      else{
+        $(".carousel-caption").css("opacity", "1");
       }
     });
   });
 
-  $(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 300) { // check if user scrolled more than 50 from top of the browser window
-        document.getElementById('header-carousel').style.visibility = 'hidden'; // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-      } else {
-        document.getElementById('header-carousel').style.visibility = 'visible'; // if not, change it back to transparent
+$(document).ready(function(){
+    $(window).scroll(function() { 
+      if ($(this.document).scrollTop() > 200) { 
+        $(".navbar-fixed-top").css("background-color", "#0d47a1");     
+      } 
+      else{
+        $(".navbar-fixed-top").css("background-color", "transparent");
       }
     });
   });
-  
+
+  $("#myModal").on('shown.bs.modal', function(){
+      $("#modal_blur").addClass("blur");//Efek Blur
+    }).blur(function(){//Menghilangkan Blur
+      $("#modal_blur").removeClass("blur");
+    });
+    
